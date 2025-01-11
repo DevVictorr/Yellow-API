f99c99239e23af9a4f06284dc55547d618ec029c
Victor <victordsantunes@gmail.com>
2025-01-11 01:30:42 -0300

    Entitys e @

--- src/main/java/com/yellowTech/yellowApi/entity/AutorEntity.java
+++ src/main/java/com/yellowTech/yellowApi/entity/AutorEntity.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,6 +27,7 @@ public class AutorEntity {
     @Column(length = 500)
     private String descricao;
 
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PostagemEntity> postagem = new ArrayList<>();
 
 }
--- src/main/java/com/yellowTech/yellowApi/entity/PostagemEntity.java
+++ src/main/java/com/yellowTech/yellowApi/entity/PostagemEntity.java
@@ -1,5 +1,52 @@
 package com.yellowTech.yellowApi.entity;
 
+import java.sql.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import javax.xml.crypto.Data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
 public class PostagemEntity {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+private String titulo;
+
+@Lob
+private String conteudo;
+
+private Date data;
+private int visualizacoes;
+
+@ManyToOne
+@JoinColumn(name = "autor_id")
+private String autor;
+
+@ManyToMany
+@JoinTable(name = "postagem_categoria", joinColumns = @JoinColumn(name = "postagem_id"),inverseJoinColumns = @JoinColumn (name = "categoria_id"))
+private List<CategoriaEntity> categorias;
+
+
+
+
 
 }
